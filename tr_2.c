01f34cd6b01fa6480c9f11767cf346def9adb97f
andylampi <lampifuo89@gmail.com>
2023-10-31 20:19:53 +0100

    fn aggiunte sortArr and printArr

--- tr_2.c
+++ tr_2.c
@@ -15,15 +15,17 @@ struct Points
 
 // Dichiaro le funzioni
 struct Points *createArr(int size);
+void sortArr(struct Points arr[], int size_arr);
+void printArr(struct Points arr[], int nmb);
 
 int main()
 {
     struct Points *arr_points;
     int size_arr; // Grandezza dell'array
     printf("Inserisci la dimensione dell'array: ");
-    scanf("%d", &size_arr); // Ottengo la grandezza dell'array
-    arr_points = createArr(size_arr);    // Creo l'array di punti
-    printf("%d", arr_points[0].x);
+    scanf("%d", &size_arr);           // Ottengo la grandezza dell'array
+    arr_points = createArr(size_arr); // Creo l'array di punti
+    sortArr(arr_points, size_arr);    // Ordino l'array
 }
 
 // Funzione che mi consente di creare l'array di punti
@@ -47,3 +49,37 @@ struct Points *createArr(int size)
     return arr_points;
 }
 
+// Funzione per riordinare l'array
+void sortArr(struct Points arr[], int size_arr)
+{
+    // Recupero il primo punto dell'array, questa variabile conterrà il punto precedente a quello attuale
+    struct Points prec = arr[0];
+    for (int i = 1; i < size_arr; i++)
+    {
+        if (prec.x > arr[i].x)
+        {
+            // Recupero il valore del punto attuale
+            struct Points point_mom = arr[i];
+            // Sostituisco il valore del punto attuale con quello precedente
+            arr[i] = prec;
+            // Sostituisco il valore del punto precedente con quello attuale
+            arr[i - 1] = point_mom;
+        }
+        prec = arr[i];
+    }
+
+    printArr(arr, size_arr);
+}
+
+
+
+
+// Funzione per visualizzare i valori all'interno dell'array
+void printArr(struct Points arr[], int nmb)
+{
+    printf("La lunghezza e' %d \n", nmb);
+    for (int i = 0; i < nmb; i++)
+    {
+        printf("%d \n", arr[i].x);
+    }
+}
